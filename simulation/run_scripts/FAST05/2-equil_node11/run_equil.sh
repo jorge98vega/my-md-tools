0ba134fbc106d7836901323b5b72afa0bbd37a34
jorge <jorgevmfa98@gmail.com>
2024-03-04 17:37:42 +0100

    Reorganize simulation directory

---
+++ simulation/run_scripts/FAST05/2-equil_node11/run_equil.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+# ===========================================
+# Slurm Header
+# ===========================================
+# SBATCH -J job_equil
+#SBATCH --partition=GPUNODE
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.error
+# SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1
+#SBATCH --get-user-env
+#SBATCH --time=999:00:00
+#SBATCH --nodelist=compute-0-11
+# SBATCH --mem-per-cpu=150
+#SBATCH --mem=5G
+# ===========================================
+#. startjob      # Do not remove this line!
+# ===========================================
+# Your Commands Go Here 
+# ===========================================
+
+# export OMP_NUM_THREADS=$NSLOTS        # SGE
+# export OMP_NUM_THREADS=$SLURM_NPROCS  # Slurm
+
+
+# Check for free GPUs
+GPU=$(nvidia-smi --query-gpu=index,utilization.gpu,memory.used --format=csv | grep "0 %" | head -n 1 | awk -F "," '{print $1}')
+
+if [ $GPU ]
+then
+  echo "GPU index = $GPU"
+  time ./MD_equil_gpu.sh $1 $GPU
+else
+  echo "ERROR: No GPUs available"
+  exit
+fi
+
+
+# ===========================================
+# End Commands
+# ===========================================
+#. endjob        # Do not remove this line!
+#--------------------------------------------
+
